a07633684c4dc545afaeadf59dfa041fc7dd1a61
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-09-22 05:49:26 +0700

    menggunakan import esmodule daripada require

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-const path = require('path')
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
