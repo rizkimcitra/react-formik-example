5d9fb66db60616ea773a7679b57338a8feb4c485
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-09-21 22:22:16 +0700

    menambahkan konfigurasi jsxinject, dan menambahkan path resolve agar dapat menggunakan absolute import

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import path from 'path'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
-    },
-    esbuild: {
-        jsxInject: `import React from 'react'`,
-    },
-    plugins: [reactRefresh()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+  },
+  plugins: [reactRefresh()],
+  esbuild: {
+    // this will make app not import react on every jsx file
+    jsxInject: `import React from 'react'`,
+  },
 })
